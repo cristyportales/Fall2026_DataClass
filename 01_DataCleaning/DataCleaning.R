59e3d5d8e49b6810f7f564561ae6fe282958b3ab
Jane Doe <jane@example.org>
2026-02-06 15:31:14 -0600

    Created scripts with outlined expectations for Project Updates 1 and 2

---
+++ 01_DataCleaning/DataCleaning.R
@@ -0,0 +1,12 @@
+##### Data Cleaning #####
+
+## By project update #1, I expect that you:
+
+## 1. have identified data sources and can read them into R
+## 2. have identified some of your questions (add them to README file)
+
+## By project update #2, I expect that you:
+
+## 3. use tidyverse or other tools to "clean" data 
+## 4. your cleaning script can be sourced 
+## 5. when you source your cleaning script, it only produces datasets and functions you need for analysis
---
+++ 02_DataExploration/DataExplorationFigures.R
@@ -0,0 +1,7 @@
+##### Data Exploration #####
+
+## By project update #2, I expect that you can:
+
+## 1. source your "clean" data to a new script
+## 2. produce exploratory figures 
+## 3. identify data distributions and transformations you might need
