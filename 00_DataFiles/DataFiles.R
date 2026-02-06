68dccc92a659d1b8829ce844b61cf7135910f30e
Jane Doe <jane@example.org>
2026-02-06 15:34:03 -0600

    Added scripts that outline expectations for what goes in each folder

---
+++ 00_DataFiles/DataFiles.R
@@ -0,0 +1,3 @@
+## Data Folder ##
+
+## This is where your raw data in the form of csv files should be
---
+++ 01_DataCleaning/Untitled.R
@@ -0,0 +1,4 @@
+##### Utilities #####
+
+## This is where code for functions you write should live
+## I expect you source functions into scripts where you'll use them from this folder
